01b4274b47db8151e9551ccb28e7ed9ac630b46f
Matt Thomas <matsinet@gmail.com>
2024-08-28 08:06:09 -0500

    API Modernization

--- server/controllers/pizzas.js
+++ server/controllers/pizzas.js
@@ -41,7 +41,7 @@ router.get("/", async (request, response) => {
     // Store the query params into a JavaScript Object
     const query = request.query;
 
-    const data = await Pizza.find(query);
+    const data = await Pizza.find(query).lean();
 
     response.json(data);
   } catch(error) {
@@ -55,7 +55,7 @@ router.get("/", async (request, response) => {
 // Get a single pizza by ID
 router.get("/:id", async (request, response) => {
   try {
-    const data = await Pizza.findById(request.params.id);
+    const data = await Pizza.findById(request.params.id).lean();
 
     response.json(data);
   } catch(error) {
@@ -71,20 +71,14 @@ router.put("/:id", async (request, response) => {
   try {
     const body = request.body;
 
-    const data = await Pizza.findByIdAndUpdate(
-      request.params.id,
-      {
-        $set: {
-          crust: body.crust,
-          cheese: body.cheese,
-          sauce: body.sauce,
-          toppings: body.toppings
-        }
-      },
-      {
-        new: true
-      }
-    );
+    const data = await Pizza.findById(request.params.id);
+
+    data.crust = body.crust;
+    data.cheese = body.cheese;
+    data.sauce = body.sauce;
+    data.toppings = body.toppings;
+
+    await data.save();
 
     response.json(data);
   } catch(error) {
@@ -100,7 +94,7 @@ router.put("/:id", async (request, response) => {
 // Delete a pizza by ID
 router.delete("/:id", async (request, response) => {
   try {
-    const data = await Pizza.findByIdAndRemove(request.params.id, {});
+    const data = await Pizza.findByIdAndRemove(request.params.id);
 
     response.json(data);
   } catch(error) {
