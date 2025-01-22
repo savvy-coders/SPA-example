868ff37c00951539db22bbacb9e29328b1d893d9
Matt Thomas <matsinet@gmail.com>
2025-01-22 14:34:12 -0600

    Remove order pizza from nav, styling updates

--- store/nav.js
+++ store/nav.js
@@ -1,7 +1,7 @@
 export default [
   { url: "/home", text: "Home" },
   { url: "/about-me", text: "About Me" },
-  { url: "/order", text: "Order a Pizza" },
+  // { url: "/order", text: "Order a Pizza" },
   { url: "/pizza", text: "Pizzas" },
   { url: "/drawings", text: "Drawings" },
   { url: "/leaflet", text: "Map" },
